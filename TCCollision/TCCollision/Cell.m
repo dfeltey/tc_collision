39ead4996b1525ab99677a0cb67c0b0f541069b4
Daniel Feltey <dfeltey@uchicago.edu>
2011-05-15 17:48:14 -0500

    small changes and additions to cell class

--- TCCollision/TCCollision/Cell.m
+++ TCCollision/TCCollision/Cell.m
@@ -18,6 +18,14 @@
     
     self.infected = NO;
     self.emitted = NO;
+    self.infection_prob = arc4random()%3;
+    self.bind_pos = ccp(0,0);
+    self.cur_pos = ccp(0,0);
+    self.replication_time = 0;
+    
+    // self.cell_sprite = 
+    
+    return self;
     
 }
 
@@ -30,4 +38,29 @@
     return distance;
 }
 
+- (void) emit_virions:(NSMutableArray *)cells
+{
+    
+}
+
+- (void) cell_motion
+{
+    float x = self.bind_pos.x + (arc4random()%(2*RADIUS) - RADIUS);
+    float y = self.bind_pos.y + (arc4random()%(2*RADIUS) - RADIUS);
+    
+    self.cur_pos = ccp(x,y); 
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
