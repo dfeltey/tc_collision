16cdcc90735377a58d7513206aaf667bec606311
Daniel Feltey <dfeltey@uchicago.edu>
2011-05-15 20:16:16 -0500

    added TC_Game class

---
+++ TCCollision/TCCollision/TC_Game.h
@@ -0,0 +1,16 @@
+//
+//  TC_Game.h
+//  TCCollision
+//
+//  Created by Daniel Feltey on 5/15/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TC_Game : NSObject {
+    
+}
+
+@end
---
+++ TCCollision/TCCollision/TC_Game.m
@@ -0,0 +1,14 @@
+//
+//  TC_Game.m
+//  TCCollision
+//
+//  Created by Daniel Feltey on 5/15/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import "TC_Game.h"
+
+
+@implementation TC_Game
+
+@end
